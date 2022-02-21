93c8cfb961f6e76d5b198371f8bf3ab55b8e7a42
Joschka Cesar <cesar@posteo.de>
2022-02-21 11:28:02 +0100

    Env removal

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -67,6 +67,7 @@ const App = () => {
       }
     } else {
       addPerson({ name: newName, number: number });
+      setNotification('Successfully added ');
     }
     setNewName('');
     setNumber('');
